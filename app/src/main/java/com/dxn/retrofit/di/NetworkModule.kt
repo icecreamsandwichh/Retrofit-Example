c900f1520a07d7db16e661fb7cf243763dcc64bc
divyansh <kushwaha.divyansh.dxn@gmail.com>
2021-07-04 13:50:01 +0530

    initial commit

---
+++ app/src/main/java/com/dxn/retrofit/di/NetworkModule.kt
@@ -0,0 +1,28 @@
+package com.dxn.retrofit.di
+
+import com.dxn.retrofit.network.NetworkService
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideNetworkService() : NetworkService {
+        return Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+            .create(NetworkService::class.java)
+    }
+
+}
--- app/src/main/java/com/dxn/retrofit/ui/MainActivity.kt
+++ app/src/main/java/com/dxn/retrofit/ui/MainActivity.kt
@@ -8,27 +8,29 @@ import com.dxn.retrofit.model.Post
 import com.dxn.retrofit.network.NetworkService
 import com.dxn.retrofit.util.ModelMapper
 import com.google.gson.GsonBuilder
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var retrofitService: NetworkService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://jsonplaceholder.typicode.com/")
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-            .build()
-            .create(NetworkService::class.java)
-
         val posts = mutableListOf<Post>()
 
         GlobalScope.launch {
-            val list = ModelMapper.networkEntityListToModelList(retrofit.get())
+            val list = ModelMapper.networkEntityListToModelList(retrofitService.get())
             posts.addAll(list)
             Log.d("DEBUG", "onCreate: $posts")
         }
